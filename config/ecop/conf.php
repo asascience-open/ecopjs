d5136f2aefaf4fbf72718279adfbd1bc39269850
Charlton Galvarino <charlton@2creek.com>
2012-04-24 04:01:12 -0400

    comment

--- config/ecop/conf.php
+++ config/ecop/conf.php
@@ -4,8 +4,8 @@
 
   // customize any values below
   $title       = 'CoastMap';
-  $getUserInfo = 'http://coastmap.com/ecop/wms.aspx?&request=GetUserInfo&version=1.1.1&';  // including trailing &
-  $wms         = 'http://coastmap.com/ecop/wms.aspx?';                                     // including trailing &
+  $getUserInfo = 'http://coastmap.com/ecop/wms.aspx?&request=GetUserInfo&version=1.1.1&';  // including trailing ? or &
+  $wms         = 'http://coastmap.com/ecop/wms.aspx?';                                     // including trailing ? or &
 
   $brandHTML   = '<table class="smallFont" width="100%"><tr><td align=center><a target=_blank href="http://www.asascience.com/"><img title="Go to ASA\'s home page" src="'.$imgSrc.'/asa.jpg"></a></td></tr></table>';
   $brandHeight = 52;
