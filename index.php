c645a415a040918ab6bac7f5348c91ebcca76347
Charlton Galvarino <charlton@2creek.com>
2012-07-13 16:07:18 -0400

    js upgrade

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
     <script type="text/javascript" src="./js/ext-3.3.0/adapter/ext/ext-base.js"></script>
     <script type="text/javascript" src="./js/ext-3.3.0/ext-all.js"></script>
     <script type="text/javascript" src="./js/ext-3.3.0/Spotlight.js"></script>
-    <script type="text/javascript" src="./js/OpenLayers-2.11-rc2/OpenLayers-closure.js"></script>
+    <script type="text/javascript" src="./js/OpenLayers-2.12/OpenLayers-closure.js"></script>
     <script type="text/javascript" src="./js/jquery/jquery.js"></script>
     <script type="text/javascript" src="./js/jquery/jquery.flot.js"></script>
     <script type="text/javascript" src="./js/jquery/jquery.flot.crosshair.js"></script>
