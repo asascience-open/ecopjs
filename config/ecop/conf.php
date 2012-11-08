5992b2d011ed29e5d08b842b216b759c9ce2b490
cgalvarino <cgalvarino@ubuntu.(none)>
2012-11-08 15:09:48 -0500

    tidying up

--- config/ecop/conf.php
+++ config/ecop/conf.php
@@ -4,6 +4,6 @@
 
   // customize any values below
   $title       = 'CoastMap';
-  $getUserInfo = 'http://coastmap.com/ecop/wms.aspx?&request=GetUserInfo&version=1.1.1&';  // including trailing ? or &
-  $wms         = 'http://coastmap.com/ecop/wms.aspx?';                                     // including trailing ? or &
+  $getUserInfo = 'http://coastmap.com/ecop/wms.aspx?request=GetUserInfo&version=1.1.1&';  // including trailing ? or &
+  $wms         = 'http://coastmap.com/ecop/wms.aspx?';                                    // including trailing ? or &
 ?>
